f9687b71a240dd2a9c1e953a01788ff8c66d9d5c
SayidolimNUU <127468262+SayidolimNUU@users.noreply.github.com>
2024-01-19 20:36:33 +0500

    Update task_7.cpp

--- src/cpp/task_7.cpp
+++ src/cpp/task_7.cpp
@@ -6,4 +6,13 @@
 
 class Problem7 {
 public:
+    int numRabbits(vector<int>& answers) {
+      unordered_map<int, int> count;
+      for(auto a : answers) { count[a] += 1; }
+      int ret = 0;
+      for(auto& kv: count) {
+        ret += (kv.second+kv.first)/(kv.first+1)*(kv.first+1);
+      }
+      return ret;
+    }
 };
