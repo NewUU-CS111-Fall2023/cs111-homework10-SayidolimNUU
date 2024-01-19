49024909a901b7da45ca802f852c555f0c3308a8
SayidolimNUU <127468262+SayidolimNUU@users.noreply.github.com>
2024-01-19 20:41:10 +0500

    Update task_8.cpp

--- src/cpp/task_8.cpp
+++ src/cpp/task_8.cpp
@@ -1,9 +1,35 @@
 /*
- * Author:
- * Date:
- * Name:
+ * Author: Sayidolimxon Usmanxonov
+ * Date: 19.01.2024
  */
 
-class Problem8 {
-public:
-};
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int n, sum = 0, k, realSum = 0;
+    cout << "Enter n:\n";
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+        realSum += arr[i];
+    }
+    for (int i = 1; i < pow(2, n); ++i) {
+        k = i;
+        sum = 0;
+        for (int j = 0; j < n; ++j) {
+            if (!k) break;
+            sum += arr[n - j - 1] * (k % 2);
+            k /= 2;
+        }
+        if (sum * 2 == realSum) {
+            cout << "True\n";
+            return 0;
+        }
+    }
+    cout << "False\n";
+    return 0;
+}
