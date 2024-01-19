f50b272bb2ab09572aeb22e31f8da9d667797063
SayidolimNUU <127468262+SayidolimNUU@users.noreply.github.com>
2024-01-19 19:10:38 +0500

    Update task_2.cpp

--- src/cpp/task_2.cpp
+++ src/cpp/task_2.cpp
@@ -1,9 +1,42 @@
 /*
- * Author:
- * Date:
- * Name:
+ * Author: Sayidolimxon Usmanxonov
+ * Date: 19.01.2024
  */
 
-class Problem2 {
-public:
-};
+
+/* Turing Machine will:
+ * state 1: if value is 1, go to right;
+ *          if value is 0, switch it to 1 and start state 2;
+ * state 2: if value is 1, go to right;
+ *          if value is 0, go to left and start state 3;
+ * state 3: change value to 0, go to left and start state 4;
+ * state 4: change value to 0, go to left and start state 5;
+ * state 3: go to left if 1, halt if 0;
+ */
+
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> v) {
+    for (auto i : v) {
+        if (!v.at(i)) v.at(i) = 1;
+    }
+    v.pop_back();
+    v.pop_back();
+    return v;
+}
+
+int main() {
+    int i;
+    vector<int> v;
+    while (cin >> i) {
+        v.push_back(i);
+    }
+    v = solution(v);
+    for (auto i : v) {
+        cout << v.at(i) << ' ';
+    }
+}
